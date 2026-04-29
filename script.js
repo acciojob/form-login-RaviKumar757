3d356622a84deeaf4538d36ea8098c40746847fe
RaviKumar757 <118956831+RaviKumar757@users.noreply.github.com>
2026-04-29 18:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault(); // stop reload
 
+  let fname = document.querySelector('input[name="fname"]').value.trim();
+  let lname = document.querySelector('input[name="lname"]').value.trim();
+
+  if (fname === "" && lname === "") {
+    alert("Please enter your name");
+    return;
+  }
+
+  let fullName = fname + " " + lname;
+
+  alert(fullName.trim());
 }
